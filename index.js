26f9cb6d2366b351393c7c3060d4e6b0fd352bc0
Shinkyuuu <codyspark@gmail.com>
2021-12-01 23:03:58 +0000

    anothr one

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const mysql = require('mysql');
-require('dotenv').config();
-const MYSQL_API_USER = process.env.MYSQL_API_USER;
-const MYSQL_API_PASS = process.env.MYSQL_API_PASS;
+// const mysql = require('mysql');
+// require('dotenv').config();
+// const MYSQL_API_USER = process.env.MYSQL_API_USER;
+// const MYSQL_API_PASS = process.env.MYSQL_API_PASS;
 
-const connection = mysql.createConnection({
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: MYSQL_API_USER,
-    password: MYSQL_API_PASS,
-    database: 'heroku_7e34334c857eca2d'
-});
+// const connection = mysql.createConnection({
+//     host: 'us-cdbr-east-04.cleardb.com',
+//     user: MYSQL_API_USER,
+//     password: MYSQL_API_PASS,
+//     database: 'heroku_7e34334c857eca2d'
+// });
 
 app.set('view engine', 'ejs');
 
