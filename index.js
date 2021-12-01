93892f687b4939e55427161f27b0c708c85a9be4
Shinkyuuu <codyspark@gmail.com>
2021-12-01 23:37:49 +0000

    another one

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
+require('env2')('.env');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mysql = require('mysql');
-require('dotenv').config();
 const MYSQL_API_USER = process.env.MYSQL_API_USER;
 const MYSQL_API_PASS = process.env.MYSQL_API_PASS;
 
-// const connection = mysql.createConnection({
-//     host: 'us-cdbr-east-04.cleardb.com',
-//     user: MYSQL_API_USER,
-//     password: MYSQL_API_PASS,
-//     database: 'heroku_7e34334c857eca2d'
-// });
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: MYSQL_API_USER,
+    password: MYSQL_API_PASS,
+    database: 'heroku_7e34334c857eca2d'
+});
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('pages/home/index.ejs');
-    // let myQuery = 'SELECT * FROM users;';
-    // connection.query(myQuery, (err, rows) => {
-    //     if (err) throw err;
-    //     else {
-    //         console.log(rows);
-    //         res.render('pages/index.ejs', { rows });
-    //     }
-    // });
+    let myQuery = 'SELECT * FROM users;';
+    connection.query(myQuery, (err, rows) => {
+        if (err) throw err;
+        else {
+            console.log(rows);
+            //res.render('pages/index.ejs', { rows });
+        }
+    });
 });
 
 app.listen(port, () => {
