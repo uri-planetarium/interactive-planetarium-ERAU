8679217c4518f8afddccc31dd91b4310103a21a1
Shinkyuuu <codyspark@gmail.com>
2021-12-02 00:19:33 +0000

    another one

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config({path: __dirname + '/.env'});
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mysql = require('mysql');
-require('dotenv').config();
 const MYSQL_API_USER = process.env.MYSQL_API_USER;
 const MYSQL_API_PASS = process.env.MYSQL_API_PASS;
 const test = process.env.test;
@@ -20,17 +20,17 @@ app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
     res.send("This is the user test " + test);
     //res.render('pages/home/index.html');
-    // let myQuery = 'SELECT * FROM users;';
-    // connection.query(myQuery, (err, rows) => {
-    //     if (err) { 
-    //         throw err;
-    //         console.log(err + " and " + process.env.MYSQL_API_USER);
-    //     }
-    //     else {
-    //         console.log(rows);
-    //         //res.render('pages/index.ejs', { rows });
-    //     }
-    // });
+    let myQuery = 'SELECT * FROM users;';
+    connection.query(myQuery, (err, rows) => {
+        if (err) { 
+            throw err;
+            console.log(err + " and " + process.env.MYSQL_API_USER);
+        }
+        else {
+            console.log(rows);
+            //res.render('pages/index.ejs', { rows });
+        }
+    });
 });
 
 app.listen(port, () => {
