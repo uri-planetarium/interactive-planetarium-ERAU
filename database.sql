499fdcb266f28bcfdaafaf556112d426100b7a8c
Shinkyuuu <codyspark@gmail.com>
2022-01-17 23:27:21 +0000

    Changes player_id generation to be more secure

--- database.sql
+++ database.sql
@@ -3,6 +3,8 @@ CREATE DATABASE planetarium_uri_db;
 DROP TABLE IF EXISTS lobbys;
 DROP TABLE IF EXISTS games;
 
+CREATE EXTENSION pgcrypto; 
+
 CREATE TABLE games (
     game_id SERIAL,
     game_code INT NOT NULL CHECK (game_code >= 0) UNIQUE,
@@ -12,7 +14,7 @@ CREATE TABLE games (
 );
 
 CREATE TABLE lobbys (
-    player_id SERIAL,
+    player_id TEXT DEFAULT ('uid_' || gen_random_uuid()),
     game_id INT,
     player_name VARCHAR(20) NOT NULL,
     player_score INT NOT NULL CHECK (player_score >= 0) DEFAULT 0,
