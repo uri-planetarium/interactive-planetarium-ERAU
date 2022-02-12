007d8e498dc56343ec9d3adb8e84cb4d486bb9bc
shinkyuuu <codyspark@gmail.com>
2022-02-12 22:30:25 +0000

    testing production code

--- player_client/src/components/joinGame/join_game_reqs.js
+++ player_client/src/components/joinGame/join_game_reqs.js
@@ -62,9 +62,10 @@ const getPlayer = async (player_id, game_code) => {
         .then(response => { 
             console.log("response: ", response);
             response.json();
+            console.log("response.json(): ", response);
+
         });
 
-        console.log("response.json(): ", response);
         
         /* If there was an error with the query, return it */
         if (!response.error) {
